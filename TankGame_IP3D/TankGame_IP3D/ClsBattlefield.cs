65b5baab58c609a07cfd0fbd612d7dc71f87266b
Unknown <luismiguelfigueiras@gmail.com>
2017-10-14 23:10:06 +0100

    Just to fix stuff

--- TankGame_IP3D/TankGame_IP3D/ClsBattlefield.cs
+++ TankGame_IP3D/TankGame_IP3D/ClsBattlefield.cs
@@ -1,21 +1,13 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Microsoft.Xna.Framework;
-//using Microsoft.Xna.Framework.Content;
-//using Microsoft.Xna.Framework.Graphics;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
-<<<<<<< Updated upstream
-//namespace TankGame_IP3D
-//{
-//    class ClsBattlefield
-//    {
-//        VertexPositionNormalTexture[] vertices;
-//        int vertexCount;
-//        VertexBuffer vertexBuffer;
-=======
+
 namespace TankGame_IP3D
 {
     class ClsBattlefield
@@ -24,60 +16,17 @@ namespace TankGame_IP3D
         VertexPositionNormalTexture v;
         int vertexCount;
         VertexBuffer vertexBuffer;
->>>>>>> Stashed changes
 
-//        int indexCount;
-//        short[] indices;
-//        IndexBuffer indexBuffer;
+        int indexCount;
+        short[] indices;
+        IndexBuffer indexBuffer;
 
-
-<<<<<<< Updated upstream
-//        BasicEffect effect;
-//        Matrix matrixTerreno=Matrix.Identity;
-//        Texture2D alturas;
-
-//        Color c;
-//        float y;
-
-//        public ClsBattlefield(GraphicsDevice device, ContentManager content)
-//        {
-//            float escala = 0.01f;
-//            alturas = content.Load<Texture2D>("heightmap");
-//            int h = alturas.Height;
-//            int w = alturas.Width;
-//            Color[] texels = new Color[h*w];
-//            alturas.GetData<Color>(texels);
-
-//            effect = new BasicEffect(device);
-//            float aspectRatio = (float)device.Viewport.Width / device.Viewport.Height;
-//            effect.View = Matrix.CreateLookAt(new Vector3(0.5f, 2.0f, 2.0f), Vector3.Zero, Vector3.Up);
-//            effect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f), aspectRatio, 1.0f, 10.0f);
-//            effect.TextureEnabled = true;
-//            effect.Texture = alturas;
-//            effect.VertexColorEnabled = true;
-//            effect.LightingEnabled = false;
-//            CreateGeometry(h, w, texels, escala, device);
-//        }
-
-//        public void CreateGeometry(int h, int w, Color[] texels, float escala, GraphicsDevice device)
-//        {
-//            vertexCount = texels.Length;
-//            vertices = new VertexPositionNormalTexture[vertexCount];
-
-//            for(int x=0; x<w; x++)
-//            {
-//                for (int z = 0; z<h; z++)
-//                {
-//                    c = texels[z * w + x];
-//                    y = c.R * escala;
-//                    vertices[z * w + x] = new VertexPositionNormalTexture(new Vector3(x, y, z), Vector3.Up, new Vector2(x % 2, z % 2));
-//                }
-//            }
-=======
         BasicEffect effect;
         Matrix matrixTerreno = Matrix.Identity;
         Texture2D alturas;
 
+        Color c;
+        float y;
         float escala = 0.05f;
         float[] alturasTextura;
 
@@ -115,12 +64,9 @@ namespace TankGame_IP3D
                     alturasTextura[z * alturas.Width + x] = y;
                 }
             }
->>>>>>> Stashed changes
+            vertexBuffer = new VertexBuffer(device, typeof(VertexPositionNormalTexture), vertices.Length, BufferUsage.None);
+            vertexBuffer.SetData<VertexPositionNormalTexture>(vertices);
 
-//            vertexBuffer = new VertexBuffer(device, typeof(VertexPositionNormalTexture), vertices.Length, BufferUsage.None);
-//            vertexBuffer.SetData<VertexPositionNormalTexture>(vertices);
-
-<<<<<<< Updated upstream
             //Usaremos strips verticais
             indexCount = alturas.Height * 2 * (alturas.Width - 1);
             indices = new short[indexCount];
@@ -132,30 +78,11 @@ namespace TankGame_IP3D
                     indices[2 * iz + 1 + ix * 2 * alturas.Height] = (short)(iz * alturas.Width + 1 + ix);
                 }
             }
-=======
-//            //Usaremos strips verticais
-//            indices = new short[indexCount];
-//            indexCount = h * 2 * (w - 1);
-//            for(int ix=0; ix < w - 1; ix++)
-//            {
-//                for(int iz=0; iz<h; iz++)
-//                {
-//                    indices[2 * iz + 0 + ix*2*h] = (short)(iz * w + ix);
-//                    indices[2 * iz + 1 + ix*2*h] = (short)(iz * w + 1 + ix);
-//                }
-//            }
->>>>>>> Stashed changes
 
-//            indexBuffer = new IndexBuffer(device, typeof(short), indices.Length, BufferUsage.None);
-//            indexBuffer.SetData<short>(indices);
-//        }
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        public void Draw(GraphicsDevice device, ContentManager content)
-=======
+            indexBuffer = new IndexBuffer(device, typeof(short), indices.Length, BufferUsage.None);
+            indexBuffer.SetData<short>(indices);
+        }
         public void Draw(GraphicsDevice device)
->>>>>>> Stashed changes
         {
             effect.World = matrixTerreno;
             effect.CurrentTechnique.Passes[0].Apply();
@@ -169,15 +96,3 @@ namespace TankGame_IP3D
         }
     }
 }
-=======
-//        public void Draw(GraphicsDevice device)
-//        {
-//            effect.World = matrixTerreno;
-//            effect.CurrentTechnique.Passes[0].Apply();
-//            device.SetVertexBuffer(vertexBuffer);
-//            device.Indices = indexBuffer;
-//            device.DrawIndexedPrimitives(PrimitiveType.TriangleStrip, 0, 0, vertexCount, 0, indexCount - 2);
-//        }
-//    }
-//}
->>>>>>> Stashed changes
